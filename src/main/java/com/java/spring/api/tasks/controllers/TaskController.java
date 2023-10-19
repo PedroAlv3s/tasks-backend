7bfd5e07a29f27ff6a6c6fccacf00d381b36b2d4
PedroAlv3s <pedro2032004@gmail.com>
2023-10-19 18:37:32 -0300

    Methods created: List and FindById.

--- src/main/java/com/java/spring/api/tasks/controllers/TaskController.java
+++ src/main/java/com/java/spring/api/tasks/controllers/TaskController.java
@@ -1,19 +1,36 @@
 package com.java.spring.api.tasks.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.spring.api.tasks.models.Task;
+import com.java.spring.api.tasks.repositorys.TaskRepository;
 
 @RestController
 @RequestMapping(value = "/tasks")
 public class TaskController {
     
+    private TaskRepository repository;
+
+    TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping(value = "/list")
     public List<Task> list() {
-        return findAll();
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/find/{id}")
+    public Task find(@PathVariable("id") Long id) {
+        Task task = repository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid task id: " + id));
+
+        return task;
     } 
 }
--- src/main/java/com/java/spring/api/tasks/repositorys/TaskRepository.java
+++ src/main/java/com/java/spring/api/tasks/repositorys/TaskRepository.java
@@ -1,7 +1,14 @@
 package com.java.spring.api.tasks.repositorys;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.java.spring.api.tasks.models.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    public List<Task> findAll();
+    public Optional<Task> findById(Long id);
+}
